3391fb772f510b1010e2970f6e89feffbab0d9af
Emmanuel Chierchie <emmchierchie@gmail.com>
2021-05-07 12:39:11 -0300

    last adjust

--- src/components/pages/StoreScreen.js
+++ src/components/pages/StoreScreen.js
@@ -3,8 +3,6 @@ import { ProductGrid } from '../products/ProductGrid';
 
 export const StoreScreen = () => {
 
-    const [ products ] = useState( [ '' ] );
-
     return (
         <>
             <section className="store-screen animate__animated animate__fadeIn">
@@ -19,15 +17,9 @@ export const StoreScreen = () => {
                     </div>
                 </header>
                 <div className="container">
-                    <div>
-                        {
-                            products.map( prod => 
-                                <ProductGrid 
-                                    key={ prod }
-                                    category={ prod } />
-                            )
-                        }
-                    </div>
+                
+                    <ProductGrid />
+                       
                 </div>
             </section>
         </>
